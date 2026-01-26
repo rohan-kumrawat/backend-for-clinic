f62704114dfec2d30d5920fa11d18f64fb7845a4
Rohan Kumrawat <rohanrkmail@gmail.com>
2026-01-26 13:11:28 +0530

    fix dashboard patient and dashboard package api

--- src/packages/packages.module.ts
+++ src/packages/packages.module.ts
@@ -5,11 +5,13 @@ import { PackagesController } from './packages.controller';
 import { Package } from './package.entity';
 import { PatientsModule } from '../patients/patients.module';
 import { Doctor } from '../doctors/doctor.entity';
+import { FinancialSummaryModule } from '../financial-summary/financial-summary.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Package,Doctor]),
     PatientsModule,
+    FinancialSummaryModule
   ],
   controllers: [PackagesController],
   providers: [PackagesService],
